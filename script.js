37809284f64959c6f74d5811af7e6b8fbd31dd06
Sean Smith <sean.smith.2009@gmail.com>
2017-12-02 08:56:46 -0800

    Progress

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 
+// Command + Enter runs code
+document.addEventListener('keydown', ({ metaKey, which }) => {
+	if (metaKey && which === 13) convert();
+});
 
+const confirmation = (message) => {
+	document.getElementById("success").innerHTML = message;
+};
+
+// Converter
 const convert = () => {
-	const { value: text } = document.getElementById('area');
-	console.log(text);
+	const testField = document.getElementById('test');
+	const messageField = document.getElementById('message');
+
+	const { value: test } = testField;
+	const { value: message } = messageField;
+
+	if (!test || !message) {
+		confirmation("Failed! Empty input!");
+		return;
+	}
+
+	const result = `
+		assert((function() {
+			${test}
+		})(), ${message});
+	`;
+
+
+	document.getElementById("dummy").value = result;
+	document.querySelector("#dummy").select();
+	document.execCommand("Copy");
+	confirmation("Success! Copied to clipboard! Rock on!");
+
+	testField.value = '';
+	messageField.value = '';
+	testField.focus();
+
 }
